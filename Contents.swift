aed6e00eb3f83b8b4de073da72f87691d9a88d74
Seth Treviño <sethtv@hotmail.com>
2019-02-22 21:20:46 -0600

    first commit

---
+++ Contents.swift
@@ -0,0 +1,58 @@
+import UIKit
+
+
+class LN {
+    var value : Int = 0
+    var next : LN?
+    var prev : LN?
+}
+
+class LRUCache {
+    var dictonary: [Int:LN] = [:]
+    let head = LN()
+    let tail = LN()
+   
+    func put (value: Int){
+        if let obj = dictonary[value] {
+            // Extraer el valor anterior y Eliminando la Referencia
+            let  prev = obj.prev
+            let  next = obj.next
+            prev?.next = next
+            next?.prev = prev
+            head.next = obj
+            
+        } else {
+            if(dictonary.isEmpty){
+                let obj = LN()
+                obj.value = value
+                head.next = obj
+                tail.prev = obj
+                dictonary[value] = obj
+            }else{
+                let obj = LN()
+                obj.next = head.next
+                obj.value = value
+                head.next = obj
+                dictonary[value] = obj
+            }
+        }
+    }
+    func get(index: Int) -> Int {
+        if let obj = dictonary[index] {
+            // Extraer el valor anterior y Eliminando la Referencia
+            let  prev = obj.prev
+            let  next = obj.next
+            prev?.next = next
+            next?.prev = prev
+            head.next = obj
+            return obj.value
+            
+        } else {
+           return 0
+        }
+    }
+}
+
+
+
+
